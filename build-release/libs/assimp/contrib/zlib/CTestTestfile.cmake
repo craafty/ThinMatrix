d2854da582b1bae929ac3205e33ee11638a7d511
craafty <the.elijah.sabin@gmail.com>
2024-11-22 00:38:29 -0600

    Fixed Everything.

---
+++ build-release/libs/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: T:/VisualStudio/ThinMatrix/libs/assimp/contrib/zlib
+# Build directory: T:/VisualStudio/ThinMatrix/build-release/libs/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
